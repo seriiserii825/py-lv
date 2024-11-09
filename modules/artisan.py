310572f5fb32d9fe35500611a9cfb5d3bb511b0e
serii burduja <seriiburduja@gmail.com>
2024-11-09 15:20:47 +0200

    feat: artisan

--- modules/artisan.py
+++ modules/artisan.py
@@ -2,11 +2,7 @@ import os
 from rich import print
 
 def artisan():
-    print("[green]1. Both")
-    print("[blue]2. ShowModel")
-    print("[green]3. Tinker")
-    print("[red]4. Develop")
-    print("[yellow]5. Back")
+    print("[green]1. Command")
     print("[red]6. Exit")
 
     option = input("Select an option: ")
@@ -14,15 +10,6 @@ def artisan():
     if option == '1':
         artisan_command = input("Artisan command like 'breez:install': ")
         os.system(f"docker-compose exec php-fpm php artisan {artisan_command}")
-    elif option == '2':
-        model_name = input("Model name like 'Flight': ")
-        os.system(f"docker-compose exec php-fpm php artisan model:show {model_name}")
-    elif option == '3':
-        os.system("docker-compose exec php-fpm php artisan tinker")
-    elif option == '4':
-        os.system("docker-compose exec php-fpm php artisan develop")
-    elif option == '5':
-        return True
     elif option == '6':
         print("[red]Good bye!")
         exit()
