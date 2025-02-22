a7239d2ac2cb1beb3683947d86ad3d0c0c5fdd8b
serii burduja <seriiburduja@gmail.com>
2025-02-22 10:01:38 +0200

    upd: artisan update

--- modules/artisan.py
+++ modules/artisan.py
@@ -1,19 +1,6 @@
 import os
-from rich import print
 
 def artisan():
-    print("[green]1. Command")
-    print("[red]6. Exit")
-
-    option = input("Select an option: ")
-
-    if option == '1':
-        artisan_command = input("Artisan command like 'breez:install': ")
-        os.system(f"docker-compose exec php-fpm php artisan {artisan_command}")
-    elif option == '6':
-        print("[red]Good bye!")
-        exit()
-    else:
-        print("[red]Invalid option")
-        artisan()
-
+    artisan_command = input("Artisan command like 'breez:install': ")
+    os.system(f"docker-compose exec php-fpm php artisan {artisan_command}")
+    exit()
