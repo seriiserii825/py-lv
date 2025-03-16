d3e4e2285b625dd9b0bbddd9150b9ccea83d3516
serii burduja <seriiburduja@gmail.com>
2025-03-16 14:51:10 +0200

    upd: component

--- modules/componentFunc.py
+++ modules/componentFunc.py
@@ -3,17 +3,18 @@ import os
 from classes.FilesHandle import FilesHandle
 def componentFunc():
     create_class = input("Create class? (y/n), by default n: ")
-    showDirPath()
+    selected_dir = getDir()
     if create_class == "y":
-        createComponent(True)
+        createComponent(True, selected_dir)
     else:
-        createComponent()
+        createComponent(False, selected_dir)
 
-def createComponent(with_class = False):
+def createComponent(with_class = False, dir = ''):
     if with_class:
         component_name = input("Enter the component name like FormInput: ")
     else:
-        component_name = input("Enter the component name like form.input: ")
+        component_name = input("Enter the component name like input: ")
+    component_name = dir + "." + component_name
     if not component_name:
         print("[red]Component name is required")
         exit()
@@ -23,7 +24,7 @@ def createComponent(with_class = False):
         command = os.system(f"docker-compose exec php-fpm php artisan make:component {component_name} --view")
     print(command)
 
-def showDirPath():
+def getDir():
     dir_path = 'resources/views/components/'
     files_handler = FilesHandle(dir_path)
     # files_handler.listDir()
@@ -34,4 +35,4 @@ def showDirPath():
         print("[red]Directory does not exist")
         exit()
     files_handler.listFiles(dir_path)
-    return dir_path
+    return selected_dir
