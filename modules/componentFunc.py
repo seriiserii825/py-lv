cdab19fb060d33dc837407aa1dfdd0d91a73bf0e
serii burduja <seriiburduja@gmail.com>
2025-03-19 15:54:08 +0200

    feat: components removed tests

--- modules/componentFunc.py
+++ modules/componentFunc.py
@@ -3,7 +3,8 @@ import os
 from classes.FilesHandle import FilesHandle
 def componentFunc():
     create_class = input("Create class? (y/n), by default n: ")
-    result = getDir()
+    files_handler = FilesHandle('resources/views/components/')
+    result = files_handler.getDir()
     selected_dir = result["selected_dir"]
     dir_path = result["dir_path"]
     if create_class == "y":
@@ -26,20 +27,3 @@ def createComponent(with_class = False, dir = '', dir_path = ''):
         os.system(f"docker-compose exec php-fpm php artisan make:component {component_name} --view")
     files_handler = FilesHandle(dir_path)
     files_handler.drawTree(dir_path)
-
-
-def getDir():
-    dir_path = 'resources/views/components/'
-    files_handler = FilesHandle(dir_path)
-    # files_handler.listDir()
-    selected_dir = files_handler.createOrChooseDirectory()
-    dir_path = files_handler.basepath + "/" + selected_dir
-    files_handler.drawTree(dir_path)
-    print(f"dir_path: {dir_path}")
-    if not os.path.exists(dir_path):
-        print("[red]Directory does not exist")
-        exit()
-    return {
-        "dir_path": dir_path,
-        "selected_dir": selected_dir
-    }
