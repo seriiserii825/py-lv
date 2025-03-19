312a72bc16838bf86851a22a9e34621967236daf
serii burduja <seriiburduja@gmail.com>
2025-03-19 15:23:22 +0200

    feat: controller with dir

--- modules/controller.py
+++ modules/controller.py
@@ -1,6 +1,8 @@
 import os
 from rich import print
 
+from classes.FilesHandle import FilesHandle
+
 
 def controller():
     print("[green]1. Simple")
@@ -10,14 +12,17 @@ def controller():
     print("[red]5. Exit")
 
     option = input("Select an option: ")
+
+    result = getDir()
+    selected_dir = result["selected_dir"]
+    controller_name = input("Controller name like 'FlightController': ")
+    controller_name = selected_dir + "/" + controller_name
+
     if option == '1':
-        controller_name = input("Controller name like 'FlightController': ")
         os.system(f"docker-compose exec php-fpm php artisan make:controller {controller_name}")
     elif option == '2':
-        controller_name = input("Controller name like 'FlightController': ")
         os.system(f"docker-compose exec php-fpm php artisan make:controller {controller_name} --resource")
     elif option == '3':
-        controller_name = input("Controller name like 'FlightController': ")
         os.system(f"docker-compose exec php-fpm php artisan make:controller {controller_name} --api")
     elif option == '4':
         return True
@@ -27,3 +32,17 @@ def controller():
     else:
         exit()
 
+def getDir():
+    dir_path = 'app/Http/Controllers'
+    files_handler = FilesHandle(dir_path)
+    selected_dir = files_handler.createOrChooseDirectory()
+    dir_path = files_handler.basepath + "/" + selected_dir
+    files_handler.drawTree(dir_path)
+    print(f"dir_path: {dir_path}")
+    if not os.path.exists(dir_path):
+        print("[red]Directory does not exist")
+        exit()
+    return {
+        "dir_path": dir_path,
+        "selected_dir": selected_dir
+    }
