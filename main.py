52b766c89d1db7f7e7fa125e9521351408879a1a
serii burduja <seriiburduja@gmail.com>
2025-04-02 13:40:27 +0300

    feat: fzf instea term_menu

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-
 from simple_term_menu import TerminalMenu
 from modules.artisan import artisan
 from modules.componentFunc import componentFunc
@@ -14,99 +13,97 @@ from modules.request import requestFunc
 from modules.resourceFunc import resourceFunc
 from rich import print
 from pyfzf.pyfzf import FzfPrompt
-
 from modules.viewFunc import viewFunc
-
 ## check for laravel project
 if not os.path.exists("artisan"):
     print("[red]This is not a laravel project")
     exit()
-    
 menu_items = [
-    "Routes",
+    "Artisan",
+    "Clear",
     "Component",
-    "Views",
-    "Trait",
     "Composer",
+    "Controller",
+    "Docker",
+    "Exit"
+    "Key Generate",
+    "Middleware",
     "Migration",
     "Model",
-    "Controller",
     "Request",
     "Resource",
-    "Middleware",
-    "Artisan",
-    "Key Generate",
-    "Docker",
-    "Clear",
-    "Exit"
+    "Routes",
+    "Trait",
+    "Views",
 ]
 
-def menu():
-    terminal_menu = TerminalMenu(menu_items)
-    menu_entry_index = terminal_menu.show()
-    if menu_entry_index is None:
-        exit()
 
-    if menu_entry_index == 0:
-        print("[blue]Routes")
-        os.system("docker-compose exec php-fpm php artisan route:list")
-        menu()
-    elif menu_entry_index == 1:
-        print("[blue]Component")
-        componentFunc()
-        menu()
-    elif menu_entry_index == 2:
-        print("[blue]Views")
-        viewFunc()
-    elif menu_entry_index == 3:
-        print("[blue]Trait")
-        newTrait()
-        menu()
-    elif menu_entry_index == 4:
-        print("[blue]Composer")
-        composer()
-        menu()
-    elif menu_entry_index == 5:
-        print("[blue]Migration")
-        migration()
-        menu()
-    elif menu_entry_index == 6:
-        print("[blue]Model")
-        model()
-        menu()
-    elif menu_entry_index == 7:
-        print("[blue]Controller")
-        controller()
-        menu()
-    elif menu_entry_index == 8:
-        print("[blue]Request")
-        requestFunc()
-        menu()
-    elif menu_entry_index == 9:
-        print("[blue]Resource")
-        resourceFunc()
-        menu()
-    elif menu_entry_index == 10:
-        print("[blue]Middleware")
-        middlewareFunc()
-        menu()
-    elif menu_entry_index == 11:
+def menu():
+    fzf = FzfPrompt()
+    menu_entry = fzf.prompt(menu_items)
+
+    if menu_entry[0] is None:
+        exit()
+    if menu_entry[0] == "Artisan":
         print("[blue]Artisan")
         artisan()
         menu()
-    elif menu_entry_index == 12:
-        print("[blue]Key Generate")
-        os.system(f"docker-compose exec php-fpm php artisan key:generate")
-        menu()
-    elif menu_entry_index == 13:
-        print("[blue]Docker")
-        dockerFunc()
-        menu()
-    elif menu_entry_index == 14:
+    elif menu_entry[0] == "Clear":
         print("[blue]Clear")
         os.system("docker-compose exec php-fpm php artisan view:clear && docker-compose exec php-fpm php artisan cache:clear && docker-compose exec php-fpm php artisan config:clear && docker-compose exec php-fpm php artisan route:clear && docker-compose exec php-fpm php artisan php artisan optimize:clear")
         menu()
-    elif menu_entry_index == 15:
+    elif menu_entry[0] == "Component":
+        print("[blue]Component")
+        componentFunc()
+        menu()
+    elif menu_entry[0] == "Composer":
+        print("[blue]Composer")
+        composer()
+        menu()
+    elif menu_entry[0] == "Controller":
+        print("[blue]Controller")
+        controller()
+        menu()
+    elif menu_entry[0] == "Routes":
+        print("[blue]Routes")
+        os.system("docker-compose exec php-fpm php artisan route:list")
+        menu()
+    elif menu_entry[0] == "Views":
+        print("[blue]Views")
+        viewFunc()
+    elif menu_entry[0] == "Trait":
+        print("[blue]Trait")
+        newTrait()
+        menu()
+    elif menu_entry[0] == "Migration":
+        print("[blue]Migration")
+        migration()
+        menu()
+    elif menu_entry[0] == "Model":
+        print("[blue]Model")
+        model()
+        menu()
+    elif menu_entry[0] == "Request":
+        print("[blue]Request")
+        requestFunc()
+        menu()
+    elif menu_entry[0] == "Resource":
+        print("[blue]Resource")
+        resourceFunc()
+        menu()
+    elif menu_entry[0] == "Middleware":
+        print("[blue]Middleware")
+        middlewareFunc()
+        menu()
+    elif menu_entry[0] == "Key Generate":
+        print("[blue]Key Generate")
+        os.system(f"docker-compose exec php-fpm php artisan key:generate")
+        menu()
+    elif menu_entry[0] == "Docker":
+        print("[blue]Docker")
+        dockerFunc()
+        menu()
+    elif menu_entry[0] == "Exit":
         print("[red]Good bye!")
         exit()
 if __name__ == "__main__":
