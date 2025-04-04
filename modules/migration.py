160583e8d8b7430e8535e7da724a357ce3c0de22
serii burduja <seriiburduja@gmail.com>
2025-04-04 13:27:03 +0300

    feat: migration

--- modules/migration.py
+++ modules/migration.py
@@ -1,70 +1,54 @@
 import os
 from rich import print
+from pyfzf.pyfzf import FzfPrompt
 
+menu_items = [
+    'Create',
+    'Run',
+    'Fresh',
+    'Rollback',
+    'Rollback_step',
+    'Reset',
+    'Exit'
+]
 
 def migration():
-    print("[green]1. Creat_migration")
-    print("[green]2. Create_migration_with_table")
-    print("[blue]3. Run_migration")
-    print("[green]4. Create_seeder")
-    print("[blue]5. Seed")
-    print("[green]6. Create_factory")
-    print("[blue]7. Migrate fresh seed")
-    print("[blue]7.1. Fresh(drop tables and re-run all migrations)")
-    print("[blue]8. Rollback")
-    print("[blue]9. Rollback_step")
-    print("[red]10. Reset")
-    print("[yellow]11. Back")
-    print("[red]12. Exit")
-    
-    option = input("Select an option: ")
-    if option == '':
-        print("[red]Good bye!")
-        exit()
-    elif option == '1':
+    fzf = FzfPrompt()
+    option = fzf.prompt(menu_items)
+
+    if option[0] == 'Create':
         migration_name = input("Migration name like 'create_flights_table': ")
         os.system(f"docker-compose exec php-fpm php artisan make:migration {migration_name}")
-        migration()
-    elif option == '2':
-        migration_name = input("Migration name like 'create_flights_table': ")
-        table_name = input("Table name like 'flights': ")
-        os.system(f"docker-compose exec php-fpm php artisan make:migration {migration_name} --table={table_name}")
-        migration()
-    elif option == '3':
+        exit()
+    elif option[0] == 'Run':
         os.system("docker-compose exec php-fpm php artisan migrate")
-        migration()
-    elif option == '4':
-        seeder_name = input("Seeder name like 'FlightsTableSeeder': ")
-        os.system(f"docker-compose exec php-fpm php artisan make:seeder {seeder_name}")
-        migration()
-    elif option == '5':
-        os.system("docker-compose exec php-fpm php artisan db:seed")
-        migration()
-    elif option == '6':
-        factory_name = input("Factory name like 'FlightsFactory': ")
-        os.system(f"docker-compose exec php-fpm php artisan make:factory {factory_name}")
-        migration()
-    elif option == '7':
+        exit()
+    elif option[0] == 'Fresh':
         os.system("docker-compose exec php-fpm php artisan migrate:fresh --seed")
-        migration()
-    elif option == '7.1':
-        os.system("docker-compose exec php-fpm php artisan migrate:fresh")
-        migration()
-    elif option == '8':
+        exit()
+    elif option[0] == 'Rollback':
         os.system("docker-compose exec php-fpm php artisan migrate:rollback")
-        migration()
-    elif option == '9':
+        exit()
+    elif option[0] == 'Rollback_step':
         step = input("How many steps you want to rollback: ")
         os.system(f"docker-compose exec php-fpm php artisan migrate:rollback --step={step}")
-        migration()
-    elif option == '10':
+        exit()
+    elif option[0] == 'Reset':
         os.system("docker-compose exec php-fpm php artisan migrate:reset")
-        migration()
-    elif option == '11':
-        return True
-    elif option == '12':
-        print("[red]Good bye!")
+        exit()
+    elif option[0] == 'Exit':
         exit()
     else:
-        print("[red]Invalid option")
-        migration()
+        exit()
+
+    # elif option[0] == '4':
+    #     seeder_name = input("Seeder name like 'FlightsTableSeeder': ")
+    #     os.system(f"docker-compose exec php-fpm php artisan make:seeder {seeder_name}")
+    #     migration()
+    # elif option[0] == '5':
+    #     os.system("docker-compose exec php-fpm php artisan db:seed")
+    #     migration()
+    # elif option[0] == '6':
+    #     factory_name = input("Factory name like 'FlightsFactory': ")
+    #     os.system(f"docker-compose exec php-fpm php artisan make:factory {factory_name}")
+    #     migration()
