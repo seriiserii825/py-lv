cf72a96a5022c27f665b6be3433516d43739f9af
serii burduja <seriiburduja@gmail.com>
2025-04-29 08:57:43 +0300

    feat: model show

--- modules/model.py
+++ modules/model.py
@@ -2,6 +2,7 @@ import os
 from pyfzf.pyfzf import FzfPrompt
 
 menu_items = [
+    'Show',
     'Create',
     'Migration',
     'Migration Seeder',
@@ -14,7 +15,11 @@ def model():
     fzf = FzfPrompt()
     option = fzf.prompt(menu_items)
 
-    if option[0] == 'Create':
+    if option[0] == 'Show':
+        model_name = input("Model name like 'Flight': ")
+        os.system(f"docker-compose exec php-fpm php artisan model:show {model_name}")
+        exit()
+    elif option[0] == 'Create':
         model_name = input("Model name like 'Flight': ")
         os.system(f"docker-compose exec php-fpm php artisan make:model {model_name}")
         exit()
